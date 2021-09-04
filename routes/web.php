0c7114f50bcda023b05c667548f30a1343e96bc9
Balba <balbartek10@gmail.com>
2021-09-04 23:30:44 +0200

    Lista uzytownikow

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HelloWorldController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
 Route::get('/hello', [HelloWorldController::class, 'show']);
 
 Auth::routes();
+
+
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
